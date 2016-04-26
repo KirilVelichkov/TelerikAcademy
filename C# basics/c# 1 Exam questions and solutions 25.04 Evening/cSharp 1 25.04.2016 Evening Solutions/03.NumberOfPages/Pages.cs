1e2cb11e14fc3467690589c6ca8afcdd1d648cb8
KirilVelichkov <kvelichkow@gmail.com>
2016-04-26 16:55:46 +0300

    EDITs

--- C# basics/c# 1 Exam questions and solutions 25.04 Evening/cSharp 1 25.04.2016 Evening Solutions/03.NumberOfPages/Pages.cs
+++ C# basics/c# 1 Exam questions and solutions 25.04 Evening/cSharp 1 25.04.2016 Evening Solutions/03.NumberOfPages/Pages.cs
@@ -8,14 +8,6 @@ class Pages
     {
         int digits = int.Parse(Console.ReadLine());
         int N = 0;
-        int[] multiplier = new int[8];
-        multiplier[1] = 1;
-        multiplier[2] = 2;
-        multiplier[3] = 3;
-        multiplier[4] = 4;
-        multiplier[5] = 5;
-        multiplier[6] = 6;
-        multiplier[7] = 7;
         int check = 0;
         int check2 = 0;
         for (int i = 1; i <= 1000000; i++)
