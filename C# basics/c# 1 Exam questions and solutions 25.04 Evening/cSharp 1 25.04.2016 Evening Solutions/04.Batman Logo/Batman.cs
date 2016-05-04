d8a84d18e1f73a33870fb0f28e25f26b1121bccc
KirilVelichkov <kvelichkow@gmail.com>
2016-05-04 19:38:37 +0300

    no idea what changed..

--- C# basics/c# 1 Exam questions and solutions 25.04 Evening/cSharp 1 25.04.2016 Evening Solutions/04.Batman Logo/Batman.cs
+++ C# basics/c# 1 Exam questions and solutions 25.04 Evening/cSharp 1 25.04.2016 Evening Solutions/04.Batman Logo/Batman.cs
@@ -7,50 +7,109 @@ class Batman
     {
         int s = int.Parse(Console.ReadLine());
         char c = char.Parse(Console.ReadLine());
-        int width = 3 * s;
-        int wings_ears = ((width - ((s - 1) * 2) - 2) / 2) - 1;
 
-        //print top;
-        for (int i = 1; i <= (s / 2); i++)
+        //print ears
+        Console.Write(new string(' ', 1));
+        Console.Write(new string(c, 1));
+        Console.Write(new string(' ', s / 4 - 1));
+        Console.Write(new string(c, 1));
+        Console.WriteLine();
+        //print head
+        for (int i = 0; i < s / 4 - 1; i++)
         {
-            Console.Write(new string(' ', i - 1));
-            Console.Write(new string(c, (s + 1) - i));
-            if (i != (s / 2))
+            Console.Write(new string(' ', 1)); //maybe not 1
+            Console.Write(new string(c, s / 4+1));
+            Console.WriteLine();
+        }
+        //print neck 1
+        for (int i = 0; i < s / 4 - 1; i++)
+        {
+            Console.Write(new string(' ', 2)); //maybe not 2
+            Console.Write(new string(c, (s / 4) - 1));
+            Console.WriteLine();
+        }
+        //print neck 2
+        for (int i = 0; i < s / 4; i++)
+        {
+            Console.Write(new string(' ', 1)); //maybe not 1
+            Console.Write(new string(c, s / 4 + 1));
+            if(i== s / 4 - 1)
             {
-                Console.Write(new string(' ', s));
+                Console.Write(new string(' ', 3)); //maybe not 3
+                Console.Write(new string(c, s / 4));
+            }
+            Console.WriteLine();
+        }
+        //print bottom with tail
+        // ### #
+        // ### #
+        // ### #
+        for (int i = 0; i < s / 4 + 2; i++)
+        {
+            Console.Write(new string(c, (s / 3) + 2));
+            if(i < s/4 + 1)
+            {
+                Console.Write(new string(' ', 2)); //maybe not 2
+                Console.Write(new string(c, 1));
             }
             else
             {
-                Console.Write(new string(' ', wings_ears));
-                Console.Write(new string(c, 1));
-                Console.Write(new string(' ', 1));
-                Console.Write(new string(c, 1));
-                Console.Write(new string(' ', wings_ears));
+                Console.Write(new string(' ', 1)); //maybe not 2
+                Console.Write(new string(c, 2));
             }
-            Console.Write(new string(c, (s + 1) - i));
-            Console.Write(new string(' ', i - 1));
             Console.WriteLine();
         }
+        //print legs
+        Console.Write(new string(' ', 1));
+        Console.Write(new string(c, (s / 3) + 3));
 
-        //print middle
-        for (int i = 1; i <= (s / 3); i++)
-        {
-            Console.Write(new string(' ', s / 2));
-            Console.Write(new string(c, (s * 2) + 1));
-            Console.Write(new string(' ', s / 2));
-            Console.WriteLine();
-        }
 
-        //print bottom
-        for (int i = 1; i <= (s / 2); i++)
-        {
-            Console.Write(new string(' ', (width - (s - (i * 2))) / 2));
-            Console.Write(new string(c, s - (i * 2)));
-            Console.Write(new string(' ', (width - (s - (i * 2))) / 2));
-            Console.WriteLine();
-        }
+        //    int s = int.Parse(Console.ReadLine());
+        //    char c = char.Parse(Console.ReadLine());
+        //    int width = 3 * s;
+        //    int wings_ears = ((width - ((s - 1) * 2) - 2) / 2) - 1;
+
+        //    //print top;
+        //    for (int i = 1; i <= (s / 2); i++)
+        //    {
+        //        Console.Write(new string(' ', i - 1));
+        //        Console.Write(new string(c, (s + 1) - i));
+        //        if (i != (s / 2))
+        //        {
+        //            Console.Write(new string(' ', s));
+        //        }
+        //        else
+        //        {
+        //            Console.Write(new string(' ', wings_ears));
+        //            Console.Write(new string(c, 1));
+        //            Console.Write(new string(' ', 1));
+        //            Console.Write(new string(c, 1));
+        //            Console.Write(new string(' ', wings_ears));
+        //        }
+        //        Console.Write(new string(c, (s + 1) - i));
+        //        Console.Write(new string(' ', i - 1));
+        //        Console.WriteLine();
+        //    }
+
+        //    //print middle
+        //    for (int i = 1; i <= (s / 3); i++)
+        //    {
+        //        Console.Write(new string(' ', s / 2));
+        //        Console.Write(new string(c, (s * 2) + 1));
+        //        Console.Write(new string(' ', s / 2));
+        //        Console.WriteLine();
+        //    }
+
+        //    //print bottom
+        //    for (int i = 1; i <= (s / 2); i++)
+        //    {
+        //        Console.Write(new string(' ', (width - (s - (i * 2))) / 2));
+        //        Console.Write(new string(c, s - (i * 2)));
+        //        Console.Write(new string(' ', (width - (s - (i * 2))) / 2));
+        //        Console.WriteLine();
+        //    }
+        //
+
     }
-
-
 }
 
