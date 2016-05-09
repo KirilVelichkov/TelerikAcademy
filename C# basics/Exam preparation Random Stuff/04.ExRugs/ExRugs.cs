b7df3e449eedf1743fc8d5b175193b9b63c141c1
KirilVelichkov <kvelichkow@gmail.com>
2016-05-10 00:57:33 +0300

    ..

--- C# basics/Exam preparation Random Stuff/04.ExRugs/ExRugs.cs
+++ C# basics/Exam preparation Random Stuff/04.ExRugs/ExRugs.cs
@@ -1,35 +1,29 @@
-﻿using System;
+﻿use strict; 
+ 
+my @arr; 
+my $s = 3; 
+ 
+my $n = 1; 
+for my $a (0 .. $s) { 
+     my $b = 0; 
+     for (0 .. $a) { 
+          $arr[$a - $b][$b++] = $n++; 
+     } 
+      
+} 
+$n = ($s + 1) ** 2; 
+for my $a (0 .. $s - 1) { 
+     my $b = 0;
+     my $bi = $s;
+     for (0 .. $a) {
+          $arr[$s - $a + $b++][$bi--] = $n--;
+     }
+}
 
-class ExRugs
-{
-    private static int i;
-
-    static void Main()
-    {
-
-       // int N = int.Parse(Console.ReadLine());
-        //int D = int.Parse(Console.ReadLine());
-
-        int width = 5;
-        int height = 5;
-        char hash = '#';
-        char[,] rug = new char[height, width];
-
-        rug[1, 1] = 'a';
-        rug[2, 3] = 'b';
-        rug[4, 4] = 'c';
-        Print(rug);
-        Print(rug);
-    }
-    static void Print(char[,] rug)
-    {
-        for (int row = 0; row < rug.GetLength(0); row++)
-        {
-            for (int col = 0; col < rug.GetLength(1); col++)
-            {
-                Console.Write(rug[row,col]);
-            }
-            Console.WriteLine();
-        }
-    }
+for my $i (0 .. $s) {
+     for my $j (0 .. $s) {
+          print $arr[$i][$j]."	";
+     }
+     print "
+";
 }
